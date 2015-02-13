90d8204bd42c05a4e351ce8235202b86f65ed756
Hack Reactor Students <accounts@hackreactor.com>
2015-02-13 15:07:33 -0800

    Sketched out pseudocode, built basic divs and general variables

--- watchout.js
+++ watchout.js
@@ -1,2 +1,35 @@
 // start slingin' some d3 here.
 
+// *Setup the environment
+var gameHeight = 450;
+var gameWidth = 700;
+var nEnemies = 30;
+var padding = 20;
+
+//score
+
+// *Setup the game board
+
+// *Axes
+
+var x = d3.scale.linear().domain([0,100]).range([0,gameWidth]);
+var y = d3.scale.linear().domain([0,100]).range([0,gameHeight]);
+
+// *Set up board as svg region
+d3.select(".board").append(svg:svg)
+  .attr('width', gameWidth)
+  .attr('height', gameHeight);
+
+
+// Scoreboard
+
+// Player
+  //  sgv class
+  //
+
+// *Enemies
+
+
+// *Render board
+
+// Play the game
