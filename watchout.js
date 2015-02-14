b54dbf973f15f23e827075d08d6a41fb2ea2e8c1
Hack Reactor Students <accounts@hackreactor.com>
2015-02-14 11:23:59 -0800

    'Turned the enemies into a picture of Nicholas Cage'

--- watchout.js
+++ watchout.js
@@ -138,28 +138,25 @@ var render = function() {
     .data(enemyObjs, function(e){return e.id});
 
   enemies.transition().duration(200)
-    .attr('cx', function( enemy ){ return axes.x(enemy.x); })
-    .attr('cy', function( enemy ){ return axes.y(enemy.y); });
+    .attr('x', function( enemy ){ return axes.x(enemy.x); })
+    .attr('y', function( enemy ){ return axes.y(enemy.y); });
 
   enemies.enter()
-    .append('svg:circle')
+    .append('image')
     .attr('class','enemy')
-    .attr('cx', function( enemy ){ return axes.x(enemy.x); })
-    .attr('cy', function( enemy ){ return axes.y(enemy.y); })
-    .attr('r', 0)
-    .transition().duration(200).attr('r', 10);
+    .attr('xlink:href', 'NickCage.png')
+    .attr('height', '30px')
+    .attr('width', '30px')
+    .attr('x', function( enemy ){ return axes.x(enemy.x); })
+    .attr('y', function( enemy ){ return axes.y(enemy.y); })
 }
 
 var checkCollisions = function() {
   d3.select("svg").selectAll(".enemy").each(function() {
-    // console.log(e.x, ', ', e.y);
     var enemy = d3.select(this);
     var player = d3.select("svg").selectAll(".player");
-    // console.log("player.x: " + player.attr("cx"));
-    // console.log("player.y: " + player.attr("cy"));
-    var dist = calcDistance(enemy.attr("cx"), enemy.attr("cy"), player.attr("cx"), player.attr("cy"));
-    //console.log("dist: " + dist);
-    if (dist < (Number(enemy.attr("r")) + Number(player.attr("r")))) {
+    var dist = calcDistance(enemy.attr("x"), enemy.attr("y"), player.attr("cx"), player.attr("cy"));
+    if (dist < 10 + Number(player.attr("r"))) {
       // console.log("data-colliding: " + player.attr('data-colliding'));
       // if (player.attr('data-colliding') === false) {
         // console.log("HIT");
@@ -195,23 +192,11 @@ var calcDistance = function(x1, y1, x2, y2) {
 var play = function(){
   var playerObj = new Player(axes.x(50), axes.y(50));
 
-  playerObj.initialize();
+  playerObj.initialize()
 
-  // d3.select('svg').append("rect")
-  //   .attr("height", 100)
-  //   .attr("width", 100)
-  //   .attr("x", 10)
-  //   .attr("y", 10)
-  //   .attr("fill", "green");
 
   var gameTurn = function (){
     render();
-    //checkCollisions();
-    // create new enemies array
-    // move enemies
-    // look for collisions
-
-
   };
 
   // make gameTurn occur every few seconds
